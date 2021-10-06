a86a5348c830d82ddaf77f69a005e744b37648c4
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2021-10-05 17:35:29 -0700

    added img id and ran npm to test if visible

--- cypress/integration/example.spec.js
+++ cypress/integration/example.spec.js
@@ -5,8 +5,8 @@ describe('Example Test', ()=>{
     });
 
     describe('HTML elements', ()=>{   
-        it('contains the alchemy logo', ()=>{
-            cy.get('.main-logo').should('be.visible');
+        it('contains the 8 ball img', ()=>{
+            cy.get('#8ball').should('be.visible');
         });
     });
 });
